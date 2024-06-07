b5547cc031d7a32754bec2db61b5190915cf7c35
HendriwGabe <162705644+HendriwGabe@users.noreply.github.com>
2024-06-07 10:52:51 -0300

    VF

--- script.js
+++ script.js
@@ -80,8 +80,13 @@ const perguntas = [
 
 let atual = 0;
 let perguntaAtual;
+let historiaFinal = "";
 
 function mostraPergunta() {
+    if(atual >= perguntas.length){
+        mostraResultado();
+        return;
+    }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
     mostraAlternativas()
@@ -92,14 +97,15 @@ function mostraAlternativas() {
     for(const alternativa of perguntaAtual.alternativas){
         const botaoAlternativas = document.createElement ("button");
         botaoAlternativas.textContent = alternativa.texto;
-        botaoAlternativas.addEventListener("click", function(){
-            atual++;
-            mostraPergunta();
-        })
+        botaoAlternativas.addEventListener("click", () => respostaSelecionada(alternativa));
         caixaAlternativas.appendChild (botaoAlternativas)
     }
 }
 
-
-
+function respostaSelecionada(opcaoSelecionada){
+    const afirmacoes = opcaoSelecionada.afirmacoes;
+    historiaFinal = afirmacoes;
+    atual++;
+    mostraPergunta();
+}
 mostraPergunta();
