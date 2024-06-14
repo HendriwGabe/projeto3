fa6b4c897f7cc4dfb86a3c6f3d350293d6b172ca
HendriwGabe <162705644+HendriwGabe@users.noreply.github.com>
2024-06-14 09:30:38 -0300

    DS

--- script.js
+++ script.js
@@ -89,6 +89,7 @@ function mostraPergunta() {
     }
     perguntaAtual = perguntas[atual];
     caixaPerguntas.textContent = perguntaAtual.enunciado;
+    caixaAlternativas.textContent = "";
     mostraAlternativas()
 }
 
@@ -104,8 +105,15 @@ function mostraAlternativas() {
 
 function respostaSelecionada(opcaoSelecionada){
     const afirmacoes = opcaoSelecionada.afirmacoes;
-    historiaFinal = afirmacoes;
+    historiaFinal += afirmacoes + " ";
     atual++;
     mostraPergunta();
 }
+
+function mostraResultado(){
+    caixaPerguntas.textContent = "Em 2049...";
+    textoResultado.textContent = "historiaFinal";
+    caixaAlternativas.textContent = "";
+}
+
 mostraPergunta();
